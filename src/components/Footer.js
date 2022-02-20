5484202c99bc01f66b533ccf07dec8f07905618f
Yusuf <yusnus2007@gmail.com>
2022-02-20 17:59:09 +0300

    array-map added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import Call from "../images/call.png";
 import Gmail from "../images/gmail.png";
 import GB from "../images/gb.png";
-import Language from "./Language";
 
 const Footer = () => {
   return (
@@ -72,7 +71,6 @@ const Footer = () => {
             <p className="text-light m-0">English</p>
             <i class="fa-solid fa-angle-down flag-arrow"></i>
           </div>
-          {/* <Language /> */}
         </div>
         <p className="copyright m-0 py-3">Copyright © 2000-2021 e’lon.uz</p>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,212 +1,24 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import Searchbar2 from "./Searchbar2.js";
-import Card from "./Card.js";
-import BigCard from "./BigCard.js";
-import Home from "../images/home1.png";
-import Steering from "../images/steering-wheel.png";
-import Banner from "../images/banner.png";
 import Slider from "./Slider.js";
+import CardList from "./SmallCard/CardList.js";
+import FilterList from "./MainFilter/FilterList.js";
+import BigCardList from "./BigCard/BigCardList.js";
 
 const Main = (props) => {
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <>
       <div className="main d-flex">
         <div className="left">
-          <div className="top-filter rounded-2 shadow-sm">
-            <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="w-100 border-0 p-0 mb-2 rounded-2 shadow-sm bg-light"
-            >
-              <div className="fliter d-flex justify-content-between align-items-center p-3">
-                <div className="d-flex align-items-center">
-                  <img className="me-2" src={Home} alt="home" />
-                  <h4 className="main-text m-0">Ko‘chmas mulk</h4>
-                </div>
-                <div
-                  className={`card-arrow ${isOpen ? "card-arrow-active" : ""}`}
-                >
-                  <i class="fa-solid fa-angle-up"></i>
-                </div>
-              </div>
-            </button>
-            <div
-              className={
-                isOpen
-                  ? "second-filter d-flex flex-column"
-                  : "second-filter show d-flex flex-column"
-              }
-            >
-              <div className="card-1 p-2 d-flex justify-content-between">
-                <h6 className="text-light m-0">Xususiy uylar</h6>
-                <h6 className="text-light m-0">(592,268)</h6>
-              </div>
-              <div className="card-2 p-2  d-flex justify-content-between">
-                <h6 className="text-secondary m-0">Kvartiralar</h6>
-                <h6 className="text-secondary m-0">(121,652)</h6>
-              </div>
-              <div className="card-3 p-2  d-flex justify-content-between">
-                <h6 className="text-secondary m-0">Yer uchastkasi</h6>
-                <h6 className="text-secondary m-0">(199,047)</h6>
-              </div>
-              <div className="card-4 p-2  d-flex justify-content-between">
-                <h6 className="text-secondary m-0">Tijorat binolari</h6>
-                <h6 className="text-secondary m-0">(602)</h6>
-              </div>
-              <div className="card-5 p-2  d-flex justify-content-between">
-                <h6 className="text-secondary m-0">Garajlar / turargohlar</h6>
-                <h6 className="text-secondary m-0">(10,962)</h6>
-              </div>
-            </div>
-          </div>
-          <div className="bottom-filter">
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3 mb-2">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-            <div className="main-card-1 d-flex justify-content-between align-items-center shadow-sm rounded-2 p-3">
-              <div className="d-flex align-items-center">
-                <img className="me-2" src={Steering} alt="" />
-                <h5 className="m-0">Transport</h5>
-              </div>
-              <i class="fa-solid fa-angle-down"></i>
-            </div>
-          </div>
+          <FilterList />
         </div>
         <div className="right w-100">
           <Searchbar2 />
           <Slider />
           <h1 className="my-4">Tavsiya etilgan e’lonlar</h1>
-          <div className="row">
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-          </div>
-          <div className="row mt-5">
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-          </div>
-          <div className="row mt-5 mb-5">
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
-            <div className="col margin-top">
-              <Card />
-            </div>
+          <div className="row p-0">
+            <CardList />
           </div>
           <div className="right-bottom mb-4">
             <div className="right-bottom-info d-flex justify-content-between align-items-center pb-3 my-2">
@@ -216,8 +28,7 @@ const Main = (props) => {
               </a>
             </div>
             <div className="big-card-holder">
-              <BigCard />
-              <BigCard />
+              <BigCardList />
             </div>
           </div>
         </div>
